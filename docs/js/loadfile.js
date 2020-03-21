ad84fb3609b20fffcc3eee0bed18d5bac191c7da
alice0de0sho <llmk114.mlove302@gmail.com>
2020-03-21 22:31:25 +0900

    [Remove]削除
    [Remove]削除
    
    docsフォルダの削除

--- docs/js/loadfile.js
+++
@@ -1,9 +0,0 @@
-const appendScript = URL => {
-  var el = document.createElement('script');
-  el.src = URL;
-  document.body.appendChild(el);
-};
-
-let loadfile = ['./js/decimal.js', './js/base.js', './js/calc.js', './js/event.js'];
-
-loadfile.forEach(value => appendScript(value));
